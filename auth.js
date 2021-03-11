53f3e800d9d1517d9af3cfba0204b20d6f0f4ca1
Kolja Pohl <kolja.pohl@pohlcon.com>
2021-03-11 12:05:18 +0100

    fixed cors issue with login

--- auth.js
+++ auth.js
@@ -1,36 +1,43 @@
+const express = require( 'express' );
+const cors = require( 'cors' );
+const app = express()
+
+app.use( cors() )
+
 const jwtSecret = 'your_jwt_secret'; //must be the same one defined within JWT Strategy!
 
-const jwt = require('jsonwebtoken'),
-passport = require('passport');
+const jwt = require( 'jsonwebtoken' ),
+    passport = require( 'passport' );
 
-require('./passport.js');
+require( './passport.js' );
 
 //generates the JWT
-let generateJWTToken = (user) => {
-    return jwt.sign(user, jwtSecret, {
+let generateJWTToken = ( user ) => {
+    return jwt.sign( user, jwtSecret, {
         subject: user.Username,
         expiresIn: '7d',
         algorithm: 'HS256'
-    });
+    } );
 }
 
 //After login
-module.exports = (router) => {
-    router.post('/login', (req, res) => {
-        passport.authenticate('local', {session: false}, (error, user, info) => {
-            if (error || !user) {
-                return res.status(400).json({
-                    message: 'Something went wrong',
-                    user: user
-                });
-            }
-            req.login(user, {session: false}, (error) => {
-                if (error) {
-                    res.send(error);
+module.exports = ( router ) => {
+    router.post( '/login',
+        cors(), ( req, res ) => {
+            passport.authenticate( 'local', { session: false }, ( error, user, info ) => {
+                if ( error || !user ) {
+                    return res.status( 400 ).json( {
+                        message: 'Something went wrong',
+                        user: user
+                    } );
                 }
-                let token = generateJWTToken(user.toJSON());
-                return res.json({user, token}); //ES6 shorthand for {user: user, token: token} !!
-            });
-        })(req, res);
-    });
+                req.login( user, { session: false }, ( error ) => {
+                    if ( error ) {
+                        res.send( error );
+                    }
+                    let token = generateJWTToken( user.toJSON() );
+                    return res.status( 200 ).json( { user, token } ); //ES6 shorthand for {user: user, token: token} !!
+                } );
+            } )( req, res );
+        } );
 }
